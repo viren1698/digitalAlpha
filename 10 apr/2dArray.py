cb791c26f7d49dd2c91e4ec910ce49cd41ecb370
viren1698 <virendra.deora1698@gmail.com>
2018-04-10 19:38:30 +0530

    Create 2dArray.py

---
+++ 10 apr/2dArray.py
@@ -0,0 +1,16 @@
+import numpy as np
+a = array([[2,0],[3,0],[3,1],[5,0],[5,1],[5,2]])
+
+print(a)
+print("conveted 2d array to 3d")
+a = np.array([a])
+print(a)
+a=a+5
+print("added 5 to each element")
+print(a)
+a=a-2
+print("subtracted 2 from each element")
+print(a)
+print("multiplied 5 to each element")
+a=a*5
+print(a)
